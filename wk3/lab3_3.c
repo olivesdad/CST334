e8af2e1d8e2abb0629d4aa33095e14b41c583321
andy <ashiraki@csumb.edu>
2022-03-19 23:14:57 -0700

    lab3

---
+++ wk3/lab3_3.c
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+
+/*Sample C program for Lab assignment 3*/
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+//main
+int main() {
+	int fds[2];
+	pipe(fds);
+	/*child 2 duplicates upstream into stdout */
+	if (fork() == 0) {
+		dup2(fds[1], 1);
+		close(fds[0]);
+		execlp("cat", "cat", "/etc/passwd", NULL);
+	}
+	/*child 1 duplicates downstream into stdin */
+	else if (fork() == 0) {
+		dup2(fds[0], 0);
+		close(fds[1]);
+		execlp("grep", "grep", "root", NULL);
+	}
+
+	else { /*parent closes both ends and wait for children*/
+		close(fds[0]);
+		close(fds[1]);
+		wait(0);
+		wait(0);
+	}
+	return 0;
+}
