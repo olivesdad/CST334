c4a715e0c8337182ad0ce91053e7b475e50fb5ce
andy <ashiraki@csumb.edu>
2022-03-27 01:50:41 -0700

    WK4Lab

---
+++ wk4/lab4.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int main ( int argc, char *argv[]){
+	char buff[12];
+	int request, temp, cacheSize;
+	bool hit;
+	int numPages=0;
+	int numFaults=0;
+	//make sure a cache size is specified
+	if (argc != 2 || atoi(argv[1]) < 1) {
+		printf("requires integer for cache size"); 
+		return 1;
+	}
+	cacheSize=atoi(argv[1]);
+	int *cache;
+	cache = (int *) calloc( cacheSize, sizeof(int));
+
+	//read line ints into buffer
+	temp = 0;
+	while (fgets(buff, 12, stdin) != NULL){
+	//this is the current page request
+		request = atoi(buff);
+		numPages++;
+	
+		//check if request is in cache
+		hit=false;
+		for (int i = 0; i < cacheSize; i++){
+			if (cache[i] == request){
+			//	printf("hit on %d \n", request);
+				hit=true;
+				break;
+			}
+		}
+		//if we didnt hit we need to shift everything up and pop the last value
+		if (!hit){
+			for (int i = cacheSize - 2; i >= 0; i--){
+				cache[i+1] = cache[i]; 
+			}
+			cache[0] = request;
+			printf("%d not found in cache \n", request);
+			numFaults++;
+		} 		
+	}	
+	
+	for(int i =0 ; i < cacheSize ; i ++){
+		printf("cache entry: %d\n", cache[i]);
+	}
+	printf("Number of pages read: %d\n", numPages);
+	printf("Number of hits: %d\n", numPages - numFaults);
+	free(cache);
+	return 0;
+}
+
